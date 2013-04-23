3c66207f5827ffc33e83d8918d38b13576bdfe1b
Tom Loredo <loredo@astro.cornell.edu>
2013-04-23 12:44:41 -0400

    Modified kernel for hard-coded data and normal rho

--- NormNorm/kernel.cu
+++ NormNorm/kernel.cu
@@ -47,6 +47,10 @@ struct wrapvec
 
 
 // constants for integration
+#DEFINE N_GH 10
+
+static __constant__ double c_rt2 = 1.4142135623730951;
+static __constant__ double c_rt2pi = 2.5066282746310002;
 static __constant__ double c_absc[] = {
 	-3.4361591188377352, -2.5327316742327906, -1.756683649299881, -1.0366108297895147,
 	-0.34290132722370503, 0.34290132722370431, 1.0366108297895129, 1.7566836492998816,
@@ -72,7 +76,15 @@ void marginals(double *theta, int d, double **features, double **sigmas, int m, 
 	if (i<n)
 	{
 		double mu = theta[0];
-		marg[i] = mu * c_wts[3] * rho(mu);
+		double var = theta[1];
+		double x;
+		double rho;
+		marg[i] = 0;
+		for (int j=0; j<N_GH; j++) {
+			x = features[0][i] + c_rt2*sigmas[0][i]*c_absc[j]
+			rho = exp(-(x-mu)**2/(2.*var))/c_rt2pi
+			marg[i] += c_wts[j]*rho;
+		}
 	}
 }
 	  
@@ -107,15 +119,21 @@ int main(void)
 {
 	// measurements
 	int n = 10; // # of items
-	int m = 2; // # of features
+	int m = 1; // # of features
 	wrapvec d_features(m,n);
 	wrapvec d_sigmas(m,n);
-	/*
+
 	unsigned int seed = 9872498732;
 	static thrust::minstd_rand rng(seed);
 	thrust::random::experimental::normal_distribution<double> dist(0.0, 1.0);
 	// thrust::generate(d_vec.begin(), d_vec.end(), dist(rng));
-	*/
+
+	for (int i=0; i<n; i++) {
+		for (int j=0; j<m; j++) {
+			d_features[j][i] = 5.;
+			d_sigmas[j][i] = 1.;
+		}
+	}
 
 	// alloc mem for marginals
 	thrust::device_vector<double> d_marg(n);
@@ -124,7 +142,7 @@ int main(void)
 	thrust::device_vector<double> d_theta(2);
 
 	// init, etc
-	d_theta[0] = 1;
+	d_theta[0] = 5;
 	d_theta[1] = 2;
 	// cudaMemcpyToSymbol(c_theta, p_theta, d_theta.size() * sizeof(*p_theta));
 	{

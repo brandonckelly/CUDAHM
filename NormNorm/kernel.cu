dc8bbe1ba9308b99ce61e9dd485a2c98c593a8da
Tamas Budavari <budavari@gmail.com>
2013-04-23 13:08:23 -0400

    Removed some obsolete code snippets in comments

--- NormNorm/kernel.cu
+++ NormNorm/kernel.cu
@@ -7,9 +7,8 @@
 #include <thrust/random/linear_congruential_engine.h>
 #include <thrust/random/normal_distribution.h>
 
-#include <thrust/generate.h>
-#include <thrust/sort.h>
-#include <thrust/copy.h>
+//#include <thrust/sort.h>
+//#include <thrust/copy.h>
 
 //#include <thrust/transform_reduce.h>
 //#include <thrust/functional.h>
@@ -91,32 +90,7 @@ void marginals(double *theta, int d, double **features, double **sigmas, int m, 
 	}
 }
 	  
-/*
-struct log_marginal
-{
-	double mu, var;
-	
-	log_marginal(double m, double v) {
-		mu = m;
-		var = v;
-	}
 
-    __host__ __device__
-    double operator()(double fhat) const { 
-		double l = 0;
-		double sigma = 1;
-
-		for (int i=0; i<10; i++)
-		{
-			double f = fhat + sigma * 1; //absc[i];
-			double d = f - mu;
-			l += exp(-d*d/var/2) / sigma * 1; // * wts[i];
-		}
-
-		return log(l);
-    }
-};
-*/
 
 int main(void)
 {
@@ -172,26 +146,3 @@ int main(void)
 	return 0;
 
 }
-
-/*
-int main(void)
-{
-    // initialize host array
-    float x[4] = {1.0, 2.0, 3.0, 4.0};
-
-    // transfer to device
-    thrust::device_vector<float> d_x(x, x + 4);
-
-    // setup arguments
-    square<float>        unary_op;
-    thrust::plus<float> binary_op;
-    float init = 0;
-
-    // compute norm
-    float norm = std::sqrt( thrust::transform_reduce(d_x.begin(), d_x.end(), unary_op, init, binary_op) );
-
-    std::cout << norm << std::endl;
-
-    return 0;
-}
-*/

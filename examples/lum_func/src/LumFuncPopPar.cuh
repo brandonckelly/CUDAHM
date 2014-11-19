df84301febd82a598ef65814141fab5b240c6763
szalaigj <szalai@complex.elte.hu>
2014-11-19 11:27:29 +0100

    Change negative infinity to min double in function LogPrior. Introduce coefficient factor to posterior density of flux | theta because when we compute estimate of theta it is very important.

--- examples/lum_func/src/LumFuncPopPar.cuh
+++ examples/lum_func/src/LumFuncPopPar.cuh
@@ -1,9 +1,9 @@
 /*
- * LumFuncPopPar.cuh
- *
- *  Created on: July 23, 2014
- *      Author: Janos M. Szalai-Gindl
- */
+* LumFuncPopPar.cuh
+*
+*  Created on: July 23, 2014
+*      Author: Janos M. Szalai-Gindl
+*/
 
 #ifndef LUMFUNCPOPPAR_CUH_
 #define LUMFUNCPOPPAR_CUH_
@@ -13,35 +13,35 @@
 extern __constant__ pLogDensPopAux c_LogDensPopAux;
 
 template <int mfeat, int pchi, int dtheta>
-class LumFuncPopPar: public PopulationPar<mfeat, pchi, dtheta>
+class LumFuncPopPar : public PopulationPar<mfeat, pchi, dtheta>
 {
 public:
 	LumFuncPopPar(int ndata, thrust::device_vector<double>& d_distData) : PopulationPar<mfeat, pchi, dtheta>(), d_distData(d_distData)
 	{
 		// grab pointer to function that compute the log-density of characteristics|theta from device
 		// __constant__ memory
-	    CUDA_CHECK_RETURN(cudaMemcpyFromSymbol(&p_logdensaux_function, c_LogDensPopAux, sizeof(c_LogDensPopAux)));
+		CUDA_CHECK_RETURN(cudaMemcpyFromSymbol(&p_logdensaux_function, c_LogDensPopAux, sizeof(c_LogDensPopAux)));
 	}
-	
+
 	// calculate the initial value of the population parameters
 	virtual void Initialize() {
 		// first set initial values
 		InitialValue();
 		InitialCholFactor();
 		// transfer initial value of theta to GPU constant memory
-	    double* p_theta = thrust::raw_pointer_cast(&h_theta[0]);
+		double* p_theta = thrust::raw_pointer_cast(&h_theta[0]);
 		CUDA_CHECK_RETURN(cudaMemcpyToSymbol(c_theta, p_theta, dtheta*sizeof(*p_theta)));
 
 		// get initial value of conditional log-posterior for theta|chi
 		double* p_chi = Daug->GetDevChiPtr(); // grab pointer to Daug.d_chi
 		double* p_logdens = thrust::raw_pointer_cast(&d_logdens[0]);
 		double* p_distData = thrust::raw_pointer_cast(&d_distData[0]);
-		logdensity_pop_aux <pchi, dtheta> <<<nBlocks,nThreads>>>(p_chi, p_logdens, p_distData, p_logdensaux_function, ndata);
+		logdensity_pop_aux <pchi, dtheta> << <nBlocks, nThreads >> >(p_chi, p_logdens, p_distData, p_logdensaux_function, ndata);
 
 		CUDA_CHECK_RETURN(cudaPeekAtLastError());
-	    CUDA_CHECK_RETURN(cudaDeviceSynchronize());
+		CUDA_CHECK_RETURN(cudaDeviceSynchronize());
 
-	    current_logdens = thrust::reduce(d_logdens.begin(), d_logdens.end());
+		current_logdens = thrust::reduce(d_logdens.begin(), d_logdens.end());
 
 		// reset the number of MCMC iterations
 		current_iter = 1;
@@ -65,17 +65,17 @@ public:
 		hvector h_proposed_theta = Propose();
 
 		// copy proposed theta to GPU constant memory
-	    double* p_proposed_theta = thrust::raw_pointer_cast(&h_proposed_theta[0]);
+		double* p_proposed_theta = thrust::raw_pointer_cast(&h_proposed_theta[0]);
 		CUDA_CHECK_RETURN(cudaMemcpyToSymbol(c_theta, p_proposed_theta, dtheta*sizeof(*p_proposed_theta)));
 
 		// calculate log-posterior of new population parameter in parallel on the device
 		const int ndata = Daug->GetDataDim();
 		double* p_logdens_prop = thrust::raw_pointer_cast(&d_proposed_logdens[0]);
 		double* p_distData = thrust::raw_pointer_cast(&d_distData[0]);
-		
-		logdensity_pop_aux <pchi, dtheta> <<<nBlocks,nThreads>>>(Daug->GetDevChiPtr(), p_logdens_prop, p_distData, p_logdensaux_function, ndata);
+
+		logdensity_pop_aux <pchi, dtheta> << <nBlocks, nThreads >> >(Daug->GetDevChiPtr(), p_logdens_prop, p_distData, p_logdensaux_function, ndata);
 		CUDA_CHECK_RETURN(cudaPeekAtLastError());
-	    CUDA_CHECK_RETURN(cudaDeviceSynchronize());
+		CUDA_CHECK_RETURN(cudaDeviceSynchronize());
 		double logdens_prop = thrust::reduce(d_proposed_logdens.begin(), d_proposed_logdens.end());
 
 		logdens_prop += LogPrior(h_proposed_theta);
@@ -88,9 +88,10 @@ public:
 			thrust::copy(d_proposed_logdens.begin(), d_proposed_logdens.end(), d_logdens.begin());
 			naccept++;
 			current_logdens = logdens_prop;
-		} else {
+		}
+		else {
 			// proposal rejected, so need to copy current theta back to constant memory
-		    double* p_theta = thrust::raw_pointer_cast(&h_theta[0]);
+			double* p_theta = thrust::raw_pointer_cast(&h_theta[0]);
 			CUDA_CHECK_RETURN(cudaMemcpyToSymbol(c_theta, p_theta, dtheta*sizeof(*p_theta)));
 			current_logdens = logdens_current;
 		}
@@ -103,22 +104,30 @@ public:
 	// NOT USED
 	virtual void SetTheta(hvector& theta, bool update_logdens = true) {
 		h_theta = theta;
-	    double* p_theta = thrust::raw_pointer_cast(&theta[0]);
+		double* p_theta = thrust::raw_pointer_cast(&theta[0]);
 		CUDA_CHECK_RETURN(cudaMemcpyToSymbol(c_theta, p_theta, dtheta * sizeof(*p_theta)));
 		if (update_logdens) {
 			// update value of conditional log-posterior for theta|chi
 			double* p_chi = Daug->GetDevChiPtr(); // grab pointer to Daug.d_chi
 			double* p_logdens = thrust::raw_pointer_cast(&d_logdens[0]);
 			double* p_distData = thrust::raw_pointer_cast(&d_distData[0]);
-			logdensity_pop_aux <pchi, dtheta> <<<nBlocks,nThreads>>>(p_chi, p_logdens, p_distData, p_logdensaux_function, ndata);
+			logdensity_pop_aux <pchi, dtheta> << <nBlocks, nThreads >> >(p_chi, p_logdens, p_distData, p_logdensaux_function, ndata);
 			CUDA_CHECK_RETURN(cudaPeekAtLastError());
 			current_logdens = thrust::reduce(d_logdens.begin(), d_logdens.end());
-            current_logdens += LogPrior(h_theta);
+			current_logdens += LogPrior(h_theta);
 		}
 	}
 
+	double min_double()
+	{
+		// const unsigned long long ieee754mindouble = 0xffefffffffffffff;
+		// return __longlong_as_double(ieee754mindouble);
+		// we choose the next double for minimal double because of technical reason:
+		// (If we summarize (more than one) ieee754mindoubles we get NaN result.)
+		return -1.797693e+250;
+	}
+
 	virtual double LogPrior(hvector theta) {
-		double negative_infinity = -std::numeric_limits<double>::infinity();
 		double gammaTheta = theta[0];
 		double lScaleTheta = theta[1];
 		double uScaleTheta = theta[2];
@@ -129,7 +138,7 @@ public:
 		}
 		else
 		{
-			result = negative_infinity;
+			result = min_double();
 		}
 		return result;
 	}
--- examples/lum_func/src/lumfunchm.cu
+++ examples/lum_func/src/lumfunchm.cu
@@ -1,11 +1,11 @@
 /*
- * lumfunchm.cu
- *
- *  Created on: July 22, 2014
- *      Author: Janos M. Szalai-Gindl
- *
- *
- */
+* lumfunchm.cu
+*
+*  Created on: July 22, 2014
+*      Author: Janos M. Szalai-Gindl
+*
+*
+*/
 
 // standard library includes
 #include <iostream>
@@ -42,17 +42,46 @@ double min_double()
 	return -1.797693e+250;
 }
 
+__device__ __host__
+double determineCoef(double gamma, double lScale, double uScale)
+{
+	double logCoef = 0;
+	if ((gamma > -1.001) && (gamma < -0.999))
+	{
+		// Euler-Mascheroni constant:
+		double gamma_const = 0.57721566490153286060651209008240243104215933593992;
+
+		// Constant from the small z expansion of the gamma function Gamma(z) :
+		double k1 = (3 * gamma_const * gamma_const + 0.5 * CR_CUDART_PI * CR_CUDART_PI) / 6.0;
+		double ratio = uScale / lScale;
+		double C0 = log(ratio + 1.0);
+		double C1 = -C0*(gamma_const + 0.5*C0);
+		double C2 = C0*(k1 + C0*(0.5*gamma_const + C0 / 6.0));
+		logCoef = logCoef - log(C0 + (gamma + 1)*(C1 + (gamma + 1)*C2));
+	}
+	else if ((gamma > -2.0) && (gamma < 0.0))
+	{
+		logCoef = logCoef - log(uScale * tgamma(gamma + 1) * (1 - 1 / pow(1 + uScale / lScale, gamma + 1)));
+	}
+	else
+	{
+		logCoef = min_double();
+	}
+	return logCoef;
+}
+
 // helper funtion for used by LogDensityPop to compute the log-density of flux | theta
 __device__ __host__
 double computeFluxLogDensWithPopPars(double gamma, double lScale, double uScale,
-									 double dist, double chiElem)
+double dist, double chiElem)
 {
-	double result;	
+	double logCoef = determineCoef(gamma, lScale, uScale);
+	double result;
 	double x = 4 * CR_CUDART_PI * dist * dist * chiElem;
 	if (x > 0)
 	{
 		double logChiDependentPart = log(1 - exp(-x / lScale)) + gamma * (log(x) - log(uScale)) - (x / uScale);
-		result = /*logCoef +*/ logChiDependentPart;
+		result = logCoef + logChiDependentPart;
 	}
 	else
 	{
@@ -70,9 +99,9 @@ double LogDensityPop(double* chi, double* theta)
 }
 
 /*
- * This function returns the logarithm of the conditional density of the characteristics given the
- * population parameter theta for a single data point, log p(chi_i | theta).
- */
+* This function returns the logarithm of the conditional density of the characteristics given the
+* population parameter theta for a single data point, log p(chi_i | theta).
+*/
 __device__
 double LogDensityPopAux(double* chi, double* theta, double dist)
 {
@@ -81,9 +110,9 @@ double LogDensityPopAux(double* chi, double* theta, double dist)
 }
 
 /*
- * Pointers to the device-side functions used to compute the conditional log-densities. These functions must be defined by the
- * user, as above.
- */
+* Pointers to the device-side functions used to compute the conditional log-densities. These functions must be defined by the
+* user, as above.
+*/
 __constant__ pLogDensMeas c_LogDensMeas = LogDensityMeas;  // log p(y_i|chi_i)
 __constant__ pLogDensPop c_LogDensPop = LogDensityPop;  // log p(chi_i|theta)
 __constant__ pLogDensPopAux c_LogDensPopAux = LogDensityPopAux;// log p(chi_i|theta)
@@ -101,46 +130,46 @@ int main(int argc, char** argv)
 
 	//ndata = 1000;
 
-    // read in measurement data from text file
-    dataAdapter.read_data(filename, meas, meas_unc, ndata, mfeat, false);
-    std::cout << "Loaded " << ndata << " data points." << std::endl;
-	
+	// read in measurement data from text file
+	dataAdapter.read_data(filename, meas, meas_unc, ndata, mfeat, false);
+	std::cout << "Loaded " << ndata << " data points." << std::endl;
+
 	std::string distFilename(argv[2]);
 	std::vector<double> distData(ndata);
-	dataAdapter.load_dist_data(distFilename,distData,ndata);
+	dataAdapter.load_dist_data(distFilename, distData, ndata);
 
 	// build the MCMC sampler
 	int niter = 1000000;
-    int nburnin = niter / 2;
-    int nchi_samples = 50;  // only keep 50 samples for the chi values to control memory usage and avoid numerous reads from GPU
-    int nthin_chi = niter / nchi_samples;
+	int nburnin = niter / 2;
+	int nchi_samples = 50;  // only keep 50 samples for the chi values to control memory usage and avoid numerous reads from GPU
+	int nthin_chi = niter / nchi_samples;
 
 	// first create pointers to instantiated subclassed DataAugmentation and PopulationPar objects, since we need to give them to the
 	// constructor for the GibbsSampler class.
 	boost::shared_ptr<DataAugmentation<mfeat, pchi, dtheta> > LFD(new LumFuncDaug<mfeat, pchi, dtheta>(meas, meas_unc));
 	thrust::device_vector<double> d_distData;
 	d_distData.resize(ndata);
-	thrust::copy(distData.begin(),distData.end(),d_distData.begin());
-	boost::shared_ptr<PopulationPar<mfeat, pchi, dtheta> > LFPP(new LumFuncPopPar<mfeat, pchi, dtheta>(ndata,d_distData));
+	thrust::copy(distData.begin(), distData.end(), d_distData.begin());
+	boost::shared_ptr<PopulationPar<mfeat, pchi, dtheta> > LFPP(new LumFuncPopPar<mfeat, pchi, dtheta>(ndata, d_distData));
 
 	// instantiate the Metropolis-within-Gibbs sampler object
-    GibbsSampler<mfeat, pchi, dtheta> Sampler(LFD, LFPP, niter, nburnin, nthin_chi);
+	GibbsSampler<mfeat, pchi, dtheta> Sampler(LFD, LFPP, niter, nburnin, nthin_chi);
 
-    // launch the MCMC sampler
-    Sampler.Run();
+	// launch the MCMC sampler
+	Sampler.Run();
 
-    // grab the samples
-    vecvec theta_samples = Sampler.GetPopSamples();  // vecvec is a typedef for std::vector<std::vector<double> >
-    std::vector<vecvec> chi_samples = Sampler.GetCharSamples();
+	// grab the samples
+	vecvec theta_samples = Sampler.GetPopSamples();  // vecvec is a typedef for std::vector<std::vector<double> >
+	std::vector<vecvec> chi_samples = Sampler.GetCharSamples();
 
-    std::cout << "Writing results to text files..." << std::endl;
+	std::cout << "Writing results to text files..." << std::endl;
 
-    // write the sampled theta values to a file. Output will have nsamples rows and dtheta columns.
-    std::string thetafile("lumfunc_thetas.dat");
-    dataAdapter.write_thetas(thetafile, theta_samples);
+	// write the sampled theta values to a file. Output will have nsamples rows and dtheta columns.
+	std::string thetafile("lumfunc_thetas.dat");
+	dataAdapter.write_thetas(thetafile, theta_samples);
 
-    // write the posterior means and standard deviations of the characteristics to a file. output will have ndata rows and
-    // 2 * pchi columns, where the column format is posterior mean 1, posterior sigma 1, posterior mean 2, posterior sigma 2, etc.
-    std::string chifile("lumfunc_chi_summary.dat");
-    dataAdapter.write_chis(chifile, chi_samples);
+	// write the posterior means and standard deviations of the characteristics to a file. output will have ndata rows and
+	// 2 * pchi columns, where the column format is posterior mean 1, posterior sigma 1, posterior mean 2, posterior sigma 2, etc.
+	std::string chifile("lumfunc_chi_summary.dat");
+	dataAdapter.write_chis(chifile, chi_samples);
 }
